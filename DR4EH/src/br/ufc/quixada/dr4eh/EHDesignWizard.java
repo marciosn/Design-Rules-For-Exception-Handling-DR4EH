25391eb40ec0554f11aac9bf260d08b87b3fb06b
Lincoln Rocha <lincolnrocha@gmail.com>
2014-05-20 18:59:15 -0300

    Regras only M can-signal E to N e M can-only-signal E to N feitas

--- DR4EH/src/br/ufc/quixada/dr4eh/EHDesignWizard.java
+++ DR4EH/src/br/ufc/quixada/dr4eh/EHDesignWizard.java
@@ -10,7 +10,7 @@ import org.designwizard.design.MethodNode;
 import org.designwizard.exception.InexistentEntityException;
 import org.designwizard.main.DesignWizard;
 
-import br.ufc.quixada.exception.DAOException;
+import br.ufc.quixada.exception.CTLException;
 
 /*
  * https://github.com/marciosn/DesignRulesForExceptionHandling.git
@@ -39,7 +39,7 @@ public class EHDesignWizard {
 		// if (ehdw.canOnlyRaise(module, CTLException.class)) {
 		// if (ehdw.OnlycanRaise(module, CTLException.class)) {
 		// if (ehdw.cannotRaise(module, DAOException.class)) {
-		if (ehdw.mustRaise(module, DAOException.class)) {
+		if (ehdw.mustRaise(module, CTLException.class)) {
 			System.out.println("Verdade!");
 		} else {
 			System.out.println("Falso!");
@@ -455,6 +455,88 @@ public class EHDesignWizard {
 		}
 
 		return cannotRaise;
+	}
+
+	public boolean onlyCanSignal(Module signalModule, Class<?> exception, Module handlerModule) {
+		boolean onlyCanSignal = false;
+
+		if (onlyCanSignal(signalModule, exception)) {
+			Set<ClassNode> handleClassNodes = new HashSet<ClassNode>();
+			if (signalModule.hasClassTypes()) {
+				handleClassNodes.addAll(convertClassTypesToClassNodes(handlerModule.getClassTypes()));
+			}
+
+			if (signalModule.hasPackageNames()) {
+				handleClassNodes.addAll(convertPackageNamesToClassNodes(handlerModule.getPackageNames()));
+			}
+
+			ClassNode exceptionClassNode;
+			try {
+				exceptionClassNode = designWizard.getClass(exception);
+			} catch (InexistentEntityException iee) {
+				throw new RuntimeException(iee);
+			}
+
+			for (ClassNode node : handleClassNodes) {
+				Set<MethodNode> methods = node.getAllMethods();
+				for (MethodNode method : methods) {
+					if (method.getCatchedExceptions().contains(exceptionClassNode)) {
+						onlyCanSignal = true;
+					}
+				}
+			}
+		}
+		return onlyCanSignal;
+
+	}
+
+	public boolean canOnlySignal(Module signalModule, Class<?> exception, Module handlerModule) {
+		Set<ClassNode> signalClassNodes = new HashSet<ClassNode>();
+		if (signalModule.hasClassTypes()) {
+			signalClassNodes.addAll(convertClassTypesToClassNodes(signalModule.getClassTypes()));
+		}
+
+		if (signalModule.hasPackageNames()) {
+			signalClassNodes.addAll(convertPackageNamesToClassNodes(signalModule.getPackageNames()));
+		}
+
+		Set<ClassNode> handleClassNodes = new HashSet<ClassNode>();
+		if (signalModule.hasClassTypes()) {
+			handleClassNodes.addAll(convertClassTypesToClassNodes(handlerModule.getClassTypes()));
+		}
+
+		if (signalModule.hasPackageNames()) {
+			handleClassNodes.addAll(convertPackageNamesToClassNodes(handlerModule.getPackageNames()));
+		}
+
+		ClassNode exceptionClassNode;
+		try {
+			exceptionClassNode = designWizard.getClass(exception);
+		} catch (InexistentEntityException iee) {
+			throw new RuntimeException(iee);
+		}
+
+		Set<MethodNode> calleeMethodNodes = new HashSet<MethodNode>();
+
+		Set<MethodNode> signalMethodNodes = new HashSet<MethodNode>();
+
+		for (ClassNode calleeClassNode : handleClassNodes) {
+			calleeMethodNodes.addAll(calleeClassNode.getCalleeMethods());
+		}
+
+		for (MethodNode calleeMethodNode : calleeMethodNodes) {
+			if (signalClassNodes.contains(calleeMethodNode.getClassNode())) {
+				signalMethodNodes.add(calleeMethodNode);
+			}
+		}
+
+		for (MethodNode signalMethodNode : signalMethodNodes) {
+			if (!(signalMethodNode.getThrownExceptions().size() == 1 && signalMethodNode.getThrownExceptions().contains(exceptionClassNode))) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 
 	private Set<ClassNode> convertClassTypesToClassNodes(Set<Class<?>> classTypes) {
